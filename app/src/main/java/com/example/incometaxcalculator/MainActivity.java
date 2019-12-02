d376390303e14b714fb13770e81900606be90548
Rajdeep Kaur <Rajdeep@12345>
2019-12-02 14:22:29 -0500

    working on Main Activity Class

--- app/src/main/java/com/example/incometaxcalculator/MainActivity.java
+++ app/src/main/java/com/example/incometaxcalculator/MainActivity.java
@@ -3,14 +3,22 @@ package com.example.incometaxcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity  extends AppCompatActivity  {
 
@@ -33,4 +41,85 @@ public class MainActivity  extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtDate= findViewById(R.id.txtDate);
+
+        // https://stackoverflow.com/questions/14933330/datepicker-how-to-popup-datepicker-when-click-on-edittext
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, monthOfYear);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                dateFormat();
+            }
+        };
+
+        txtDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(MainActivity.this, date, calendar
+                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+        txtAge=findViewById(R.id.txtAge);
+        rgGender=findViewById(R.id.rgGender);
+        rbMale=findViewById(R.id.rbMale);
+        rbFemale=findViewById(R.id.rbFemale);
+        rbOthers=findViewById(R.id.rbOthers);
+        rgGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                if (rbMale.isChecked())
+                    Toast.makeText(MainActivity.this, "You Chose Male", Toast.LENGTH_SHORT).show();
+                if (rbFemale.isChecked())
+                    Toast.makeText(MainActivity.this, "You Chose Female", Toast.LENGTH_SHORT).show();
+                if (rbOthers.isChecked())
+                    Toast.makeText(MainActivity.this, "You Chose Others", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //submit button
+        btnSubmit=findViewById(R.id.btnCalculate);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String age=dateFormat();
+                if(Integer.parseInt(age)<18){
+                    btnSubmit.setAlpha(.5f);
+                    btnSubmit.setClickable(false);
+                }
+                else{
+                    Intent intent=new Intent(MainActivity.this,Tax_detailsActivity.class);
+                    startActivity(intent);
+                }}
+        });
+    }
+    private String dateFormat() {
+        String myFormat = "dd-MMM-yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        txtDate.setText(sdf.format(calendar.getTime()));
+
+        LocalDate l = LocalDate.of(calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        LocalDate now = LocalDate.now(); //gets localDate
+        Period diff = Period.between(l, now); //difference between the dates is calculated
+        System.out.println(diff.getYears() + "years" + diff.getMonths() + "months" + diff.getDays() + "days");
+
+        String n1=String.valueOf(diff.getYears());
+        String n2=String.valueOf(diff.getMonths());
+        String n3=String.valueOf(diff.getDays());
+        String age="Age: "+n1+"Years"+n2+"Months"+n3+"Days";
+
+        txtAge.setText(age);
+        return  n1;
+
+
+
+    }
+
 }
