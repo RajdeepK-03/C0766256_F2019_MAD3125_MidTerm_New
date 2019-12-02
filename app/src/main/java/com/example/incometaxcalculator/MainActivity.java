2843ce89ede4bd9427f42df1e4c398768e9600f3
Rajdeep Kaur <Rajdeep@12345>
2019-12-02 14:19:22 -0500

    working on Main Activity Class

--- app/src/main/java/com/example/incometaxcalculator/MainActivity.java
+++ app/src/main/java/com/example/incometaxcalculator/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.incometaxcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
 
+public class MainActivity  extends AppCompatActivity  {
+
+    final Calendar calendar = Calendar.getInstance();
+    TextView txtDate;
+    RadioGroup rgGender;
+    RadioButton rbMale;
+    RadioButton rbFemale;
+    RadioButton rbOthers;
+    TextView txtFullName;
+    EditText edtFname;
+    EditText edtLname;
+    Button btnSubmit;
+    TextView txtAge;
+    DatePickerDialog datePickerDialog;
+
+    int dDay;
+    int dMonth;
+    int dYear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
